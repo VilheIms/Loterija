937be9474725e8d5cea3c06412826a53ef50db8b
Vilhelms <boysepticeye@gmail.com>
2025-12-16 09:20:26 +0200

    Skaitļi tiek ģenerēti, bet tikai 1 reizi un bumbiņas netiek vēl izvēlētas par laimīgo

--- src/steks/Uzd2.java
+++ src/steks/Uzd2.java
@@ -1,9 +1,42 @@
 package steks;
 
+import java.util.Random;
+import java.util.Stack;
+
+import javax.swing.JOptionPane;
+
 public class Uzd2 {
 
 	public static void main(String[] args) {
-		int a;
+		Stack<Integer> LoterijasBumbas = new Stack<>();
+		Random rand = new Random();
+		String izvele;
+		int bumbinas, laime;
+		String[] darbibas = {"Ģenerēt bumbiņas", "Apskatīt laimīgās bumbiņas", "Apturēt"};
+		do {
+			izvele = (String)JOptionPane.showInputDialog(null, "Izvēlies darbību", "Darbību saraksts", JOptionPane.QUESTION_MESSAGE, null, darbibas, darbibas[0]);
+		switch(izvele) {
+		case "Ģenerēt bumbiņas":
+				for(int i = 0; i < 4; i++) {
+				for(i = 0; i<10; i++) {
+					laime = rand.nextInt(9);
+					LoterijasBumbas.push(laime);
+					}
+				
+				}
+				System.out.println(LoterijasBumbas);
+				break;
+		case "Apskatīt laimīgās bumbiņas":
+			if(!LoterijasBumbas.isEmpty())
+				JOptionPane.showMessageDialog(null, "Stekā esošā vārda garums:"+LoterijasBumbas.size(), "Paziņojums", JOptionPane.INFORMATION_MESSAGE);
+			else
+				JOptionPane.showMessageDialog(null, "Steks ir tukšs!", "Paziņojums", JOptionPane.INFORMATION_MESSAGE);
+			break;
+		case "Apturēt":
+			JOptionPane.showMessageDialog(null, "Programma apturēta!", "Informācija", JOptionPane.INFORMATION_MESSAGE);
+			break;
+		}
+		}while(!izvele.equals("Apturēt"));
 	}
 
 }
