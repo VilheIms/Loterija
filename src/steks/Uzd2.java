0cf94517a3753dcb946cb853c57142fe9b897a07
Vilhelms <boysepticeye@gmail.com>
2025-12-16 09:55:43 +0200

    Programmai ir tagad 60 sekunžu gaidīšana, tomēr nevar apskatīties bumbiņas tajā laikā. Vēlāk es izdomāšu, kā to uzlabot.

--- src/steks/Uzd2.java
+++ src/steks/Uzd2.java
@@ -2,6 +2,8 @@ package steks;
 
 import java.util.Random;
 import java.util.Stack;
+import java.util.Timer;
+import java.util.concurrent.TimeUnit;
 
 import javax.swing.JOptionPane;
 
@@ -11,13 +13,23 @@ public class Uzd2 {
 		Stack<Integer> LoterijasBumbas = new Stack<>();
 		Stack<Integer> UzvarasBumbas = new Stack<>();
 		Random rand = new Random();
+		Timer timer = new Timer();
 		String izvele;
-		int bumbinas, laime, laimiga = 0;
+		
+		
+		
+		
+		
+		int bumbinas, laime, laimiga = 0, laiks = 0;
 		String[] darbibas = {"Ģenerēt bumbiņas", "Apskatīt laimīgās bumbiņas", "Apturēt"};
 		do {
 			izvele = (String)JOptionPane.showInputDialog(null, "Izvēlies darbību", "Darbību saraksts", JOptionPane.QUESTION_MESSAGE, null, darbibas, darbibas[0]);
 		switch(izvele) {
 		case "Ģenerēt bumbiņas":
+			if(laiks != 0) {
+				System.out.println("Nav vēl laiks beidzies! Atlikušais laiks: "+laiks);
+				break;
+			}else {
 				for(int i = 0; i < 3; i++) {
 					for(int j = 0; j<10; j++) {
 					laime = rand.nextInt(9);
@@ -27,15 +39,20 @@ public class Uzd2 {
 				LoterijasBumbas.clear();
 				UzvarasBumbas.push(laimiga);
 				}
-				System.out.println(laimiga);
-				System.out.println(UzvarasBumbas);
+				JOptionPane.showMessageDialog(null, "Laimīgās bumbiņas: "+UzvarasBumbas, "Paziņojums", JOptionPane.INFORMATION_MESSAGE);
 				UzvarasBumbas.clear();
+				try {
+					Thread.sleep(60000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
 				break;
 		case "Apskatīt laimīgās bumbiņas":
 			if(!LoterijasBumbas.isEmpty())
 				JOptionPane.showMessageDialog(null, "Stekā esošā vārda garums:"+LoterijasBumbas.size(), "Paziņojums", JOptionPane.INFORMATION_MESSAGE);
 			else
-				JOptionPane.showMessageDialog(null, "Steks ir tukšs!", "Paziņojums", JOptionPane.INFORMATION_MESSAGE);
+				JOptionPane.showMessageDialog(null, "Nav laimīgās bumbiņas!", "Paziņojums", JOptionPane.INFORMATION_MESSAGE);
 			break;
 		case "Apturēt":
 			JOptionPane.showMessageDialog(null, "Programma apturēta!", "Informācija", JOptionPane.INFORMATION_MESSAGE);
